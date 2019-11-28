a1e54d9ea7a391e91111294110ed17d89fe9f1dd
sebastian <s.kurzynowski@nogravity.io>
2019-11-28 08:49:54 +0100

    Updates

---
+++ precache-manifest.04733add0f4ece1f89e1bd24dbb0df02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd528330278ddc18c38b62c8512809b2",
+    "url": "/react-example/index.html"
+  },
+  {
+    "revision": "973b789803dc1e30fac6",
+    "url": "/react-example/static/css/main.ae7f76e2.chunk.css"
+  },
+  {
+    "revision": "148134ccf325d8f0ee8a",
+    "url": "/react-example/static/js/2.fc7e3b6c.chunk.js"
+  },
+  {
+    "revision": "973b789803dc1e30fac6",
+    "url": "/react-example/static/js/main.d0cf0df1.chunk.js"
+  },
+  {
+    "revision": "cedf958112ffca35ec16",
+    "url": "/react-example/static/js/runtime-main.ddc8d267.js"
+  }
+]);
